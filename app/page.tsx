896b2dcd7ad3473f275fe6a06d43c652124b20a1
mu7ammad1 <153460583+mu7ammad1@users.noreply.github.com>
2025-04-29 17:47:13 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
       </div>
       <div className="flex flex-col items-center justify-center gap-4 w-full">
         <h3 className="flex justify-start w-full">Inspiration</h3>
-        <Card className="w-full max-w-full gap-2 shadow-none justify-center items-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <Card className="w-full max-w-full gap-2 shadow-none justify-center items-center grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           <PinterestLayout
             src={[
               "https://heroui.com/images/hero-card-complete.jpeg",
