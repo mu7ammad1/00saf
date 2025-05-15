20e312ff7cd9f2dbdbb25a78354bd7afcaf7dfcd
mu7ammad1 <153460583+mu7ammad1@users.noreply.github.com>
2025-05-15 09:17:59 +0200

    first commit

--- app/design/[slug]/page.jsx
+++ app/design/[slug]/page.jsx
@@ -1,10 +1,6 @@
-export default async function Page({
-    params,
-  }) {
-    const { slug } = await params
-    return <div>My Post: {slug}</div>
-  }
+/* eslint-disable prettier/prettier */
+export default async function Page({ params }) {
+  const { slug } = await params;
 
-
-
-
+  return <div>My Post: {slug}</div>;
+}
